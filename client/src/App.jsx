2de8d94dacb8fa006042fdfb88f60506561486f0
yaqin66 <ainul.8815@gmail.com>
2024-12-02 17:05:44 +0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dashboard from './pages/Dashboard'
+import LoginForm from './auth/LoginForm'
+
+const App = () => {
+  return (
+    <div>
+    <LoginForm/>
+    {/* <Dashboard/> */}
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/auth/LoginForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <div>
+        <div className='border p-10'>
+            <div>Username</div>
+            <div>Password</div>
+        </div>
+    </div>
+  )
+}
+
+export default LoginForm
